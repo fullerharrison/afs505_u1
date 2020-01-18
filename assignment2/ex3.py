27febb0000db60d970871ae5ab0ccc092ae06a85
Harrison Fuller <harrison.fuller@wsu.edu>
2020-01-18 14:32:20 -0800

    "Second commit from work pc assignment2"

---
+++ assignment2/ex3.py
@@ -0,0 +1,26 @@
+print("I will now count my chickens:") # prints phrase
+
+print("Hens", float(25+30/6)) #calculates the number of Hens
+print("Roosters", float(100-25*3%4)) # calculates the number of Roosters
+
+print("Now I will count the eggs:") # prints phrase
+
+print(float(3+2+1-5+4%2-1/4+6)) # Computation
+
+
+print("Is it true that 3+2 < 5-7?") #Phrase asking a # QUESTION:
+
+print(float(3+2<5-7)) # Prints Computation
+
+
+print("What is 3+2?", float(3+2)) # Phrase and Computation
+print("What is 5-7?", float(5-7)) # Phrase and Computation
+
+
+print("Oh, that's why it's False.") # Prints Phrase
+
+print("How about some more.") # Prints Phrase
+
+print("Is it greater?", 5>-2) # Phrase and conditional statement
+print("Is it greater or equal?", 5>=-2) # Phrase and conditional statement
+print("Is it less or equal?", 5<= -2) # Phrase and conditional statement
