e512ce686e0c1d0dedba87f58972c91ab3404b38
fullerharrison <harrison.fuller@wsu.edu>
2020-02-14 17:20:54 -0800

    test

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 from sys import argv
+def print_grid(empty_grid):
+    output = ""
+    for row in range(30) :
+        for cell in range(80):
+            output += empty_grid[row][cell]
+    output += '\n'
+
 def main(*argv):
     empty_grid = [[0] * 80 for i in range(30)] # makes grid
     print_grid(empty_grid)
 
-def print_grid(empty_grid):
-    output = ""
-    for row_index, row in enumerate(empty_grid) :
-        for column_index, cell in enumerate(row):
-            output += empty_grid[row_index][column_index]
-    output += '\n'
+
 main(*argv)
